a51487b0382fe2c15f85062f2d6d2d9cc6d25364
Abdallah-SE <abdalla.a911@yahoo.com>
2022-02-22 00:44:26 +0200

    Display the day when he/she get max value

--- high-treatment.php
+++ high-treatment.php
@@ -1,4 +1,6 @@
 <?php
+//$bourse_values = [5,7,21,2,8,9,4,19];
+
 function setHighValue(array $boursa){
     if (!is_array($boursa)){
         return;
@@ -11,7 +13,7 @@ function setHighValue(array $boursa){
     return $tem_exchange;
 }
 
-$bourse_values = [5,7,21,2,8,9,4,20];
+$bourse_values = [5,7,21,2,8,9,4,19];
 $str_values = setHighValue($bourse_values);
 $str_values = rtrim($str_values, ',');
 $arr = explode(',', $str_values);
@@ -20,14 +22,32 @@ function printArray ( array $arr ) {
         echo $arr[$i].' , ';
     }
 }
+
+function shiftArray(array $arr):array{
+    if(!(is_array($arr))){
+        return "";
+    }
+    $tem_arr = [];
+    for($i=1; $i<=count($arr); $i++){
+        $tem_arr[$i-1] = $arr[count($arr)-$i];
+    }
+    return $tem_arr;
+}
+$arranged_arr = shiftArray($arr);
+/// in the first day he/she not gain anything
+array_unshift($arranged_arr, '0');
+var_dump($arranged_arr);
 function getMaxNum(array $arrNum){
     $max = $arrNum[0];
+    $index = 0;
     for($i=0; $i<count($arrNum); $i++){
         if($max < $arrNum[$i]){
             $max = $arrNum[$i];
+            $index = $i;
         }
     }
-    return $max;
+    $index +=1;
+    return $max . " IN day: " .$index;
 }
 echo 'In the following array: ';
-echo printArray($bourse_values).' the max value is ' .(getMaxNum($arr));
+echo printArray($bourse_values).' the max value is ' .(getMaxNum($arranged_arr));
